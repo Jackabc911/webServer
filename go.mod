e78d143c473dfdf72e33ed9c8a692d579a2ee51c
Jackabc911 <jackabc911@gmail.com>
2025-01-24 07:09:29 +0300

    new web server

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/Jackabc911/standartServer
+
+go 1.23.5
+
+require (
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/auth0/go-jwt-middleware v1.0.1 // indirect
+	github.com/form3tech-oss/jwt-go v3.2.5+incompatible // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+)
---
+++ internal/app/middleware/middleware.go
@@ -0,0 +1,18 @@
+package middleware
+
+import (
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/form3tech-oss/jwt-go"
+)
+
+var (
+	SecretKey      []byte      = []byte("SuperDryMegaSecureWord100k300")
+	emptyValidFunc jwt.Keyfunc = func(token *jwt.Token) (interface{}, error) {
+		return SecretKey, nil
+	}
+)
+
+var JwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: emptyValidFunc,
+	SigningMethod:       jwt.SigningMethodHS256,
+})
